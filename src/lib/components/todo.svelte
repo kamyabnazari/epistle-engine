b111bd667f985e00d07fe9cf762765098d9812a8
Kamyab Nazari <kamyabnazari@icloud.com>
2023-05-13 22:16:55 +0200

    Adding tailwindcss prettier support

---
+++ src/lib/components/todo.svelte
@@ -0,0 +1,13 @@
+<form class="my-6">
+	<div class="flex-col mb-2 text-sm">
+		<label class="mb-2 font-bold text-gray-800" for="todo">Todo</label>
+		<input
+			class="p-2 border border-gray-200 rounded-lg shadow appearance-none focus:outline-none focus:boder-gray-500"
+			type="text"
+			id="name"
+			name="todo"
+			placeholder="Your tasks?"
+		/>
+	</div>
+	<button type="submit">Add</button>
+</form>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,11 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script>
+	import ToDo from '../lib/components/todo.svelte';
+</script>
+
+<div class="grid h-screen place-content-center">
+	<h1 class="text-8xl">Hello world!</h1>
+	<ToDo />
+</div>
 
 <style lang="postcss">
 	:global(html) {
