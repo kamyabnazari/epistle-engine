a191182c518de5ef7a3506f0ecd1ccf865d268ec
Kamyab Nazari <kamyabnazari@icloud.com>
2023-05-21 00:29:31 +0200

    Adding gpt plugin fastapi and dockerfile

--- README.md
+++ README.md
@@ -78,3 +78,25 @@ docker-compose up
 ```
 
 > To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
+
+### ChatGPT Plugin
+
+To install the required packages for this plugin, run the following command:
+
+```bash
+pip install -r requirements.txt
+```
+
+To run the plugin, enter the following command:
+
+```bash
+python main.py
+```
+
+Once the local server is running:
+
+1. Navigate to https://chat.openai.com.
+2. In the Model drop down, select "Plugins" (note, if you don't see it there, you don't have access yet).
+3. Select "Plugin store"
+4. Select "Develop your own plugin"
+5. Enter in `localhost:5003` since this is the URL the server is running on locally, then select "Find manifest file".
--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,52 @@
-from typing import Union
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse, JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+import uvicorn
 
-from fastapi import FastAPI
+class Todo(BaseModel):
+    todo: str
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://chat.openai.com"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+_TODOS = {}
+
+@app.post("/todos/{username}", response_model=Todo)
+async def add_todo(username: str, todo: Todo):
+    _TODOS.setdefault(username, []).append(todo.todo)
+    return todo
+
+@app.get("/todos/{username}", response_model=List[str])
+async def get_todos(username: str):
+    return _TODOS.get(username, [])
+
+@app.delete("/todos/{username}")
+async def delete_todo(username: str, todo: Todo):
+    if username in _TODOS and todo.todo in _TODOS[username]:
+        _TODOS[username].remove(todo.todo)
+        return JSONResponse(content='OK', status_code=200)
+    raise HTTPException(status_code=404, detail="Todo not found")
+
+@app.get("/logo.png")
+async def logo():
+    return FileResponse('logo.png', media_type='image/png')
+
+@app.get("/.well-known/ai-plugin.json")
+async def ai_plugin():
+    return FileResponse('./.well-known/ai-plugin.json', media_type='application/json')
+
+@app.get("/openapi.yaml")
+async def openapi():
+    return FileResponse('openapi.yaml', media_type='text/yaml')
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5003)
