5563043a433da42a41190099a9b08414bd1fe1f4
Kamyab Nazari <kamyabnazari@icloud.com>
2023-05-27 10:50:44 +0200

    Adding create and read pages

--- frontend/src/routes/dashboard/file-create/+page.svelte
+++ frontend/src/routes/dashboard/file-create/+page.svelte
@@ -1,1 +1,39 @@
-<h1>File Create</h1>
+<div class="hero bg-base-100 min-h-full">
+	<div class="hero-content">
+		<div class="card p-16 shadow-lg">
+			<div class="flex flex-col gap-16">
+				<div class="flex justify-center">
+					<h1 class="text-2xl font-bold">Create a new file</h1>
+				</div>
+				<div class="flex justify-center">
+					<div class="flex-grow">
+						<ul class="steps w-full">
+							<li class="step step-primary">Generate Text</li>
+							<li class="step">Preview</li>
+							<li class="step">Done</li>
+						</ul>
+					</div>
+				</div>
+				<div class="flex justify-center">
+					<div class="form-control">
+						<label for="" class="label">
+							<span class="label-text">What do you want to generate?</span>
+						</label>
+						<textarea
+							class="textarea textarea-bordered h-48 w-96"
+							placeholder="I want an essay about birds..."
+						/>
+					</div>
+				</div>
+				<div class="flex flex-row justify-center">
+					<div class="flex-auto">
+						<a href="/dashboard"><button class="btn btn-ghost">Cancel</button></a>
+					</div>
+					<div>
+						<a href="/dashboard"><button class="btn btn-primary">Next</button></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- frontend/src/routes/dashboard/file-read/+page.svelte
+++ frontend/src/routes/dashboard/file-read/+page.svelte
@@ -1,22 +1,36 @@
-<div class="flex flex-col gap-12">
-	<div class="flex flex-row justify-center">
-		<h1 class="text-2xl font-bold">File Upload</h1>
-	</div>
-	<div class="flex flex-row justify-center">
-		<div class="flex-grow">
-			<ul class="steps w-full">
-				<li class="step step-primary">Upload</li>
-				<li class="step">Preview</li>
-				<li class="step">Done</li>
-			</ul>
-		</div>
-	</div>
-	<div class="flex flex-row justify-center">
-		<div class="form-control w-full max-w-xs">
-			<label for="file" class="label">
-				<span class="label-text">Pick a file</span>
-			</label>
-			<input type="file" class="file-input file-input-bordered w-full max-w-xs" />
+<div class="hero bg-base-100 min-h-full">
+	<div class="hero-content">
+		<div class="card p-16 shadow-lg">
+			<div class="flex flex-col gap-16">
+				<div class="flex flex-row justify-center">
+					<h1 class="text-2xl font-bold">Read a new file</h1>
+				</div>
+				<div class="flex flex-row justify-center">
+					<div class="flex-grow">
+						<ul class="steps w-full">
+							<li class="step step-primary">Upload</li>
+							<li class="step">Preview</li>
+							<li class="step">Done</li>
+						</ul>
+					</div>
+				</div>
+				<div class="flex flex-row justify-center">
+					<div class="form-control w-full max-w-xs">
+						<label for="file" class="label">
+							<span class="label-text">Pick a file</span>
+						</label>
+						<input type="file" class="file-input file-input-bordered w-full max-w-xs" />
+					</div>
+				</div>
+				<div class="flex flex-row justify-center">
+					<div class="flex-auto">
+						<a href="/dashboard"><button class="btn btn-ghost">Cancel</button></a>
+					</div>
+					<div>
+						<a href="/dashboard"><button class="btn btn-primary">Next</button></a>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
