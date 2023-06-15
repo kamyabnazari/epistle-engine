c203816360691a5ba41d709a71d808416aeee29a
Kamyab Nazari <kamyabnazari@icloud.com>
2023-06-15 18:06:11 +0200

    Send the correct file casing

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import Navbar from '$lib/components/Navbar.svelte';
-	import Footer from '$lib/components/Footer.svelte';
+	import Footer from '$lib/components/footer.svelte';
 	import { page } from '$app/stores';
 </script>
 
