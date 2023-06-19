8bf589977a287474761971b40972d41cf1cba3fb
Kamyab Nazari <kamyabnazari@icloud.com>
2023-06-19 22:40:38 +0200

    Trying out example

--- frontend/src/routes/dashboard/+page.svelte
+++ frontend/src/routes/dashboard/+page.svelte
@@ -1,12 +1,26 @@
 <script lang="ts">
+	import { env } from '$env/dynamic/public';
 	import ActionCards from '$lib/components/ActionCards.svelte';
 	import FileTable from '$lib/components/FileTable.svelte';
 	import Stats from '$lib/components/Stats.svelte';
+
+	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
+
+	let data = writable(null);
+
+	onMount(async () => {
+		const res = await fetch(`${env.PUBLIC_BACKEND_URL}/api`);
+		data = await res.json();
+	});
 </script>
 
 <div class="flex flex-col gap-12">
 	<div class="flex justify-center">
 		<h1 class="text-2xl font-bold">Dashboard</h1>
+	</div>
+	<div class="flex justify-center">
+		<h1 class="text-2xl font-bold">{JSON.stringify(data)}</h1>
 	</div>
 	<div class="flex">
 		<Stats />
