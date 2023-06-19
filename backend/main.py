63d6910a00557d790f18ec7efc3f393aa8b4f8d8
Kamyab Nazari <kamyabnazari@icloud.com>
2023-06-19 23:02:13 +0200

    Trying out cors settings

--- backend/main.py
+++ backend/main.py
@@ -58,7 +58,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[pocketbase_url, frontend_public_url],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
