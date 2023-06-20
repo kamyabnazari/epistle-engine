a7f674e823930b4a3cd40135094284890b3a8cd9
Kamyab Nazari <kamyabnazari@icloud.com>
2023-06-20 13:09:28 +0200

    Try out fetch example

--- frontend/src/routes/dashboard/+page.server.ts
+++ frontend/src/routes/dashboard/+page.server.ts
@@ -1,8 +1,22 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { env } from '$env/dynamic/public';
+import axios from 'axios';
+import http from 'http';
+import https from 'https';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	if (!locals.user) {
 		throw redirect(303, '/login');
 	}
+
+	const res = await axios({
+		url: `${env.PUBLIC_BACKEND_URL}/api`,
+		method: 'get',
+		headers: { 'Content-Type': 'application/json' },
+		httpAgent: new http.Agent({ family: 4 }), // Force IPv4
+		httpsAgent: new https.Agent({ family: 4 }) // Force IPv4
+	});
+
+	console.log(res.data);
 };
