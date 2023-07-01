d9f11ee67348390dde8ef33e5e19e5a3618c9ed9
Kamyab Nazari <kamyabnazari@icloud.com>
2023-07-01 19:48:30 +0200

    Fixing dashboard styling

--- frontend/src/routes/dashboard/+page.svelte
+++ frontend/src/routes/dashboard/+page.svelte
@@ -12,21 +12,23 @@
 	});
 </script>
 
-<div class="flex flex-col gap-8">
-	<div class="flex justify-center">
-		<h1 class="text-2xl font-bold">Dashboard</h1>
-	</div>
-	<div class="flex">
-		<StatsBar />
-	</div>
-	<div class="flex">
-		<ActionCardStats />
-	</div>
-	<div class="flex">
-		<ActionCards />
-	</div>
+<div class="mx-auto flex min-h-full max-w-7xl flex-col gap-8">
+	<div class="flex flex-col gap-8">
+		<div class="flex justify-center">
+			<h1 class="text-2xl font-bold">Dashboard</h1>
+		</div>
+		<div class="flex">
+			<StatsBar />
+		</div>
+		<div class="flex">
+			<ActionCardStats />
+		</div>
+		<div class="flex">
+			<ActionCards />
+		</div>
 
-	<div class="flex flex-grow">
-		<FileTable />
+		<div class="flex flex-grow">
+			<FileTable />
+		</div>
 	</div>
 </div>
