9bcff582b046a86debdcff8208c918be30b8484b
Kamyab Nazari <kamyabnazari@icloud.com>
2023-07-02 13:19:47 +0200

    adding charts and changing for stats

---
+++ frontend/src/routes/dashboard/stats/all-chunks-topics/stats-all-chunks-topics-data.ts
@@ -0,0 +1,14 @@
+export default [
+	{
+		id: 'Introduction',
+		value: 525
+	},
+	{
+		id: 'Conclusion',
+		value: 1231
+	},
+	{
+		id: 'Personal',
+		value: 34
+	}
+];
---
+++ frontend/src/routes/dashboard/stats/all-docs-topics/stats-all-docs-topics-data.ts
@@ -0,0 +1,26 @@
+export default [
+	{
+		id: 'Sports',
+		value: 525
+	},
+	{
+		id: 'Scientific',
+		value: 1231
+	},
+	{
+		id: 'Topic',
+		value: 34
+	},
+	{
+		id: 'Topic',
+		value: 213
+	},
+	{
+		id: 'Topic',
+		value: 1231
+	},
+	{
+		id: 'Topic',
+		value: 51
+	}
+];
